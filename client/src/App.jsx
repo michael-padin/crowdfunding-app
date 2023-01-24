5f9626c7988fccd78bb6e3d37c54fd17941f34d8
Your Name <padinmichael201@gmail.com>
2023-01-25 01:49:27 +0800

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+
+/* import styles */
+import "./index.css";
+
+// react router dom
+import { Route, Routes } from "react-router-dom";
+
+// components
+import { Navbar, Sidebar } from "./components";
+
+// pages
+import { Home, CampaignDetails, CreateCampaign, Profile } from "./pages";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="relative sm:p-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+      <div className="flex-1 max-sm:w-full max-w-[1200px] max-auto sm:pr-5">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
